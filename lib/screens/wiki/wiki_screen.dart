f334b8735b5a99f9a24df4513be1fc725d596fab
Umut Y. Çalışkan <umutyasin2001@gmail.com>
2024-02-23 10:54:10 +0300

    Add camera functionality and UI enhancements

--- lib/screens/wiki/wiki_screen.dart
+++ lib/screens/wiki/wiki_screen.dart
@@ -1,12 +1,20 @@
+import 'package:camera/camera.dart';
 import 'package:eco_echo/models/wiki/categories.dart';
 import 'package:eco_echo/widgets/wiki/waste_category_item.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../../models/camera.dart';
+import '../camera/camera.dart';
+import 'detail_screen.dart';
 
 class WikiScreen extends StatelessWidget {
   const WikiScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final cameraModel = context.read<CameraModel>();
+    CameraDescription camera = cameraModel.camera;
     List<WasteCategories> categoryItems = [
       WasteCategories(
         icon: Image.asset(
@@ -60,14 +68,74 @@ class WikiScreen extends StatelessWidget {
 
     return ListView(
       children: [
-        Container(
-          margin: const EdgeInsets.all(16),
-          height: 120,
-          width: double.infinity,
-          decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(10),
-              color: const Color(0xffADF5D6)),
-          child: Text("asd"),
+        TextButton(
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => CameraScreen(camera: camera),
+              ),
+            );
+          },
+          child: Container(
+              margin: const EdgeInsets.all(16),
+              height: 84,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: const Color(0xff26C881)),
+              child: Row(
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  Container(
+                    margin: const EdgeInsets.only(left: 16.0),
+                    padding: const EdgeInsets.all(4),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: Colors.white,
+                    ),
+                    child: Image.asset(
+                      "assets/google-gemini-icon.png",
+                      height: 40,
+                      width: 40,
+                    ),
+                  ),
+                  const SizedBox(width: 72),
+                  const Flexible(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        SizedBox(
+                          height: 20,
+                        ),
+                        Text(
+                          "Recycle",
+                          style: TextStyle(
+                            fontSize: 26,
+                            color: Colors.white,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            Text(
+                              "powered by Gemini",
+                              style: TextStyle(
+                                fontSize: 12,
+                                color: Colors.white,
+                              ),
+                            ),
+                            SizedBox(width: 8),
+                          ],
+                        ),
+                        SizedBox(height: 2)
+                      ],
+                    ),
+                  ),
+                ],
+              )),
         ),
         const Padding(
           padding: EdgeInsets.all(16.0),
